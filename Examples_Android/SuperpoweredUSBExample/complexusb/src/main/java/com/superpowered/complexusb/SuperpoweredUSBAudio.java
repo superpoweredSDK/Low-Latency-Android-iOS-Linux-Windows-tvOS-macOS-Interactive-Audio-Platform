d8af1b3e710100ffe481fe0860fc9c82e595d89f
blade <balazsbanto@gmail.com>
2022-12-06 10:35:52 +0100

    Improvements to the android usb-sample apps: changed mutability flag for pending intent, fixed OnConnectReturn switch case, added permission request for devices already connected and fetched during the check() function call

--- Examples_Android/SuperpoweredUSBExample/complexusb/src/main/java/com/superpowered/complexusb/SuperpoweredUSBAudio.java
+++ Examples_Android/SuperpoweredUSBExample/complexusb/src/main/java/com/superpowered/complexusb/SuperpoweredUSBAudio.java
@@ -21,7 +21,7 @@ public class SuperpoweredUSBAudio {
     public SuperpoweredUSBAudio(Context c, SuperpoweredUSBAudioHandler h) {
         context = c;
         handler = h;
-        permissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
+        permissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_MUTABLE);
 
         BroadcastReceiver usbReceiver = new BroadcastReceiver() {
             @Override
@@ -61,16 +61,23 @@ public class SuperpoweredUSBAudio {
     private void addUSBDevice(UsbDevice device) {
         UsbManager manager = (UsbManager)context.getSystemService(Context.USB_SERVICE);
         if (manager == null) return;
+
+        if (!manager.hasPermission(device)) {
+            manager.requestPermission(device, permissionIntent);
+            return;
+        }
+
         UsbDeviceConnection connection = manager.openDevice(device);
         if (connection != null) {
             int id = device.getDeviceId();
             switch (onConnect(id, connection.getFileDescriptor(), connection.getRawDescriptors())) {
-                case 1: if (handler != null) handler.onUSBAudioDeviceAttached(id); break; // Audio device.
-                case 2: if (handler != null) handler.onUSBMIDIDeviceAttached(id); break; // MIDI device.
-                case 3: // Audio and MIDI device.
+                case 0: // Audio and MIDI device.
                     if (handler != null) handler.onUSBAudioDeviceAttached(id);
                     if (handler != null) handler.onUSBMIDIDeviceAttached(id);
                     break;
+                case 1: if (handler != null) handler.onUSBAudioDeviceAttached(id); break; // Audio device.
+                case 2: if (handler != null) handler.onUSBMIDIDeviceAttached(id); break; // MIDI device.
+                case 3: break; // Found no audio or MIDI features for this device.
             }
         }
     }
