486b121c807411058c7ff8bfe0d6ae852c11cac2
Balazs Banto <balazsbanto@gmail.com>
2023-08-16 09:53:26 +0200

    cleanup

--- Examples_ReactNative/SuperpoweredPlayerWithEffects/android/app/src/main/java/com/superpoweredplayerwitheffects/SuperpoweredModule.java
+++ Examples_ReactNative/SuperpoweredPlayerWithEffects/android/app/src/main/java/com/superpoweredplayerwitheffects/SuperpoweredModule.java
@@ -2,13 +2,9 @@ package com.superpoweredplayerwitheffects;
 
 import androidx.annotation.NonNull;
 
-import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import java.util.Map;
-import java.util.HashMap;
 
 public class SuperpoweredModule extends ReactContextBaseJavaModule {
 
