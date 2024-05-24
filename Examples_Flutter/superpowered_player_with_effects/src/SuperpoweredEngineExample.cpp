f3c5eb71334851a530de0e472fbfeb14e4573d2d
Ivan Nador <ivan.nador@synervoz.com>
2024-05-24 11:48:04 +0200

    Changed Flutter/React Native progressive download url, downgraded Android target SDK because it would need more refactoring to account for API 34

--- Examples_Flutter/superpowered_player_with_effects/src/SuperpoweredEngineExample.cpp
+++ Examples_Flutter/superpowered_player_with_effects/src/SuperpoweredEngineExample.cpp
@@ -16,7 +16,7 @@
 SuperpoweredEngineExample::SuperpoweredEngineExample() {
   player = std::make_unique<Superpowered::AdvancedAudioPlayer>(48000, 0);
   player->loopOnEOF = true;
-  
+
   flanger = std::make_unique<Superpowered::Flanger>(48000);
   flanger->enabled = true;
 
@@ -29,7 +29,7 @@ SuperpoweredEngineExample::~SuperpoweredEngineExample() {
 }
 
 void SuperpoweredEngineExample::play() {
-  player->open("https://docs.superpowered.com/audio/samples/splice/SO_PF_74_string_phrase_soaring_Gb.mp3");
+  player->open("https://superpowered.s3.amazonaws.com/SO_PF_74_string_phrase_soaring_Gb.mp3");
   player->play();
 }
 
